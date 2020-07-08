656b53ef0614457791d1b8bd64ba006e3107b723
Cameron Kirby <cameronevankirby@gmail.com>
2020-07-08 17:13:39 -0500

    Added MVP

--- server.js
+++ server.js
@@ -1,13 +1,32 @@
 const express = require('express');
+const userRouter = require('./users/userRouter')
 
 const server = express();
 
+// Middleware
+// Global
+server.use(express.json())
+server.use(logger)
+
 server.get('/', (req, res) => {
-  res.send(`<h2>Let's write some middleware!</h2>`);
+    res.send(`<h2>Let's write some middleware!</h2>`);
 });
 
-//custom middleware
+server.use('/user', userRouter)
 
-function logger(req, res, next) {}
+
+function logger(req, res, next) {
+    // Global middleware that logs to the console the following information about each request: 
+    // request method, 
+    // request url, 
+    // and a timestamp
+    const timestamp = Date()
+    console.log(`-! REQUEST INFO !-`)
+    console.log(`Request Method: ${req.method}`)
+    console.log(`Request URL: ${req.url}`)
+    console.log(`Timestamp: ${timestamp.toString()}`)
+    console.log(`------------------`)
+    next()
+}
 
 module.exports = server;
